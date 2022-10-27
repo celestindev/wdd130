b00fc9d4dba1314540f45ef5deac7ca2b7cdfc1e
celestindev <celestingerald31@gmail.com>
2022-10-27 12:43:04 -0400

    updated css

---
+++ wwr/week5.py
@@ -0,0 +1,6 @@
+keep_commandments = input("Will you keep God's commandments? Yes or No?")
+
+if keep_commandments == "Yes":
+   print("You get liberty and eternal life!! 🙂")
+else:
+   print("You get captivity and death. 😔")
